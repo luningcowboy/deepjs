b1d26d2c9a260b69b0a6c69cfbbc4e5a05f8d02b
luningcowboy <luningcowboy@gmail.com>
2020-03-20 19:28:19 +0800

    add time format

---
+++ test/test-timeformat.js
@@ -0,0 +1,19 @@
+
+function num2Second(num){
+    var ret = '';
+    if(num >= 0 && num <= 60){
+        if(num < 10) ret = '0'+num;
+        else ret = num + '';
+    }
+    return ret;
+}
+function timeFormat(time){
+    var ret = '';
+    if(time <= 3600){
+        var left = parseInt(time / 60);
+        var right = time - left * 60;
+        ret = num2Second(left) + ':' + num2Second(right);
+    }
+    return ret;
+}
+console.log(timeFormat(328));
