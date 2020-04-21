395e7e0938eb8990b89e51f17e2c6a61926f0a70
luningcowboy <luningcowboy@gmail.com>
2020-04-21 11:17:22 +0800

    add filter

--- tools/filter.js
+++ tools/filter.js
@@ -1,9 +1,32 @@
 function Filter(){}
 
-function.prototype.getFilterResult = function(callFuncs, input){
+/**
+ * getFilterResult
+ *
+ * @param callFuncs [function]
+ * @param input
+ * @returns {undefined}
+ */
+Filter.prototype.getFilterResult = function(callFuncs, input){
     var ret = input;
     for(var i = 0; i < callFuncs.length; i++){
         ret = callFuncs[i](ret);
+    }
+    return ret;
+};
+/**
+ * getFilterResult2
+ *
+ * @param callFuncs array[{func:function, target:target}]
+ * @param input
+ * @returns {undefined}
+ */
+Filter.prototype.getFilterResult2 = function(callFuncs, input){
+    var ret = input;
+    for(var i = 0; i < callFuncs.length; i++){
+        var func = callFuncs.func;
+        var target = callFuncs.target;
+        ret = func.prototype.apply(target, ret);
     }
     return ret;
 };
